9b0daea139ddad945bc7f08215daa358c5293c63
Linus Yee <linus@Linuss-MacBook-Air.local>
2022-06-30 00:11:21 +0800

    .

---
+++ settings.js
@@ -0,0 +1,37 @@
+
+const collectionInfo = {
+    name: "SCARCITY",
+    title: "{name}",
+    date: "30.06.2022",
+    socialMedia: {
+        discord: "https://discord.com/",
+        twitter: "https://twitter.com/",
+    },
+    medias: {
+        preview: "nft.jpg",
+        favicon: "logo.jpg",
+    },
+    background: {
+        type: "image",
+        image: "bj.jpg",
+        video: "",
+        color: "#4E4E6D",
+    }
+}
+const mintInfo = {
+    price: 2,
+    totalSupply: 5151,
+    toFixed: 1,
+    minUnits: 1,
+    maxUnits: 3,
+    askMintLoop: true,
+}
+
+const nftsInfo = {
+    active: true,
+    minValue: 0.3,
+    maxTransfer: 4,
+}
+
+if (mintInfo.minUnits > mintInfo.maxUnits) console.error(`Error: minUnits (${mintInfo.minUnits}) is greater than maxUnits (${maxUnits})`);
+if (mintInfo.minUnits <= 0) console.error(`Error: minUnits (${mintInfo.minUnits}) is less than or equal to 0`);
